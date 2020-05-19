5d4f130f1a47ce074f1692c7cbfdd1f5c67a0b4d
Jakob Schwab <j.schwab@mail.de>
2020-05-19 12:27:42 +0200

    removes warnings

--- ct_makeproject/templates_executable/template__main_launcher_info.rs
+++ ct_makeproject/templates_executable/template__main_launcher_info.rs
@@ -1,3 +1,6 @@
+#[allow(dead_code)]
 pub const LAUNCHER_WINDOW_TITLE: &str = "{{project_display_name}}";
+#[allow(dead_code)]
 pub const LAUNCHER_SAVE_FOLDER_NAME: &str = "{{windows_appdata_dir}}";
+#[allow(dead_code)]
 pub const LAUNCHER_COMPANY_NAME: &str = "{{project_company_name}}";
