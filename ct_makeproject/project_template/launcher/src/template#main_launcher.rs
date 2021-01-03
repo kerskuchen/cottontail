b9a91e621bc9c5cad04e08f81a6c833d6f97ebf2
Jakob Schwab <j.schwab@mail.de>
2021-01-03 13:08:11 +0100

    updates main_launcher template

--- ct_makeproject/project_template/launcher/src/template#main_launcher.rs
+++ ct_makeproject/project_template/launcher/src/template#main_launcher.rs
@@ -4,3 +4,18 @@ pub const LAUNCHER_WINDOW_TITLE: &str = "{{project_display_name}}";
 pub const LAUNCHER_SAVE_FOLDER_NAME: &str = "{{windows_appdata_dir}}";
 #[allow(dead_code)]
 pub const LAUNCHER_COMPANY_NAME: &str = "{{project_company_name}}";
+
+mod game;
+
+fn main() {
+    ct_lib_game::start_mainloop::<game::GameState>();
+}
+
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+
+#[cfg(target_arch = "wasm32")]
+#[wasm_bindgen(start)]
+pub fn run_main() {
+    main();
+}
